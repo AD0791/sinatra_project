db79db2e579e25568e9397a962e192e9874aaed3
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-16 21:08:32 -0400

    we enable the session and the method helpers inside the application controller as a good practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,27 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+     set :session_secret, "hotel_security"
   end
 
   get "/" do
+    
+    #binding.pry
+    
     erb :index
+  end
+
+
+  # helper method
+  helpers do
+    def logged_in?
+      !!session[:client_id]
+    end
+
+    def current_user
+      Client.find(session[:client_id])
+    end
   end
 
 end
