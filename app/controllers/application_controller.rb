71631458221b27d5a00853dcffe4d4563493d846
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-17 09:49:50 -0400

    add the rack setup in the application_controller. we add some condition about the login situation in the root routes. to verify if the user is already log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 require './config/environment'
-
+require 'rack-flash'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -7,12 +7,14 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
      set :session_secret, "hotel_security"
+    use Rack::Flash
   end
 
   get "/" do
-    
+    if logged_in?
+      redirect "/clients/#{current_user.slug}"
+    end
     #binding.pry
-    
     erb :index
   end
 
