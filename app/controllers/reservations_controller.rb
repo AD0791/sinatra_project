6369035789b651a06ea951f884ef573184abe9e2
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-19 16:24:52 -0400

    set everything for the reservation routes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,8 @@
 class ReservationsController < ApplicationController
-    
+    get '/reservations/new' do
+        if !logged_in?
+            redirect '/'
+        end
+        erb :'/reservations/new'
+    end
 end
