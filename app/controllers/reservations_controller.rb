77ef57e388b41f9d582eb48103b2ef794dc48a6d
Alexandro Disla <alexandrodisla@hotmail.com>
2019-08-19 21:17:02 -0400

    The client can now add new erservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,4 +5,16 @@ class ReservationsController < ApplicationController
         end
         erb :'/reservations/new'
     end
+
+    post '/reservations' do
+        #binding.pry
+        params[:date] != "" && params[:occupancy]!="" && params[:room] != "" && params[:number_of_day] != "" && params[:bed_size] != ""
+        params[:client_id] = current_user.id
+        @reservation = Reservation.create(params)
+        flash[:message] = "Your reservation  has been added"
+        redirect "/clients/#{@reservation.client.slug}"
+        if @reservation.client_id != current_user.id
+            redirect "/clients/#{current_user.slug}"
+        end
+    end
 end
